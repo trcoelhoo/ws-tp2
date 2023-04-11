6083d6712b946c9b50b02c0ec38a190459176695
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2023-04-11 13:40:08 +0100

    graphdb

---
+++ GraphDB/graphdb-data/data/settings.js
@@ -0,0 +1,32 @@
+{
+  "import.server" : { },
+  "import.local" : {
+    "books;;books.nt" : {
+      "name" : "books.nt",
+      "status" : "DONE",
+      "message" : "Imported successfully in 14s.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : "file:/uploaded/generated/books.nt",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "fada8441-5837-4401-a9b5-cad4d451761c",
+      "timestamp" : 1681147043887,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      },
+      "requestIdHeadersToForward" : " X-Request-Id: 3c2bce18-b4e9-5356-89c5-495c247358f5"
+    }
+  },
+  "properties" : {
+    "current.location" : ""
+  }
+}
