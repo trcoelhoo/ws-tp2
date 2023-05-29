1f901444760210dca194d465637654851d9173c1
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2023-05-29 18:45:21 +0100

    Update tmp.py

--- Converter/tmp.py
+++ Converter/tmp.py
@@ -2,10 +2,10 @@
 with open("books.nt", "r") as file:
     with open("newbooks.nt", "w") as file02:
         for row in file:
-            if row.startswith("<http://books.com/publishers/") and "ns#type" in row:
+            if row.startswith("<http://books.com/publishers/"):
                 comps = row.split()
-                comps[2] = '<http://books.com/publishers#Publisher>.'
-                nwrow = " ".join(comps)
-                file02.write(f"{nwrow}\n")
+                id = comps[0].strip(">").strip("<").split("/")[-1]
+                comps[0] = f"publisher:{id}"
+                file02.write(f"{' '.join(comps)}\n")
             else:
                 file02.write(row)
