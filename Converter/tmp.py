f456f76257860e2cee1252a56478622e139060e4
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2023-05-29 21:30:12 +0100

    x

--- Converter/tmp.py
+++ Converter/tmp.py
@@ -2,10 +2,11 @@
 with open("books.nt", "r") as file:
     with open("newbooks.nt", "w") as file02:
         for row in file:
-            if row.startswith("<http://books.com/publishers/"):
-                comps = row.split()
-                id = comps[0].strip(">").strip("<").split("/")[-1]
-                comps[0] = f"publisher:{id}"
-                file02.write(f"{' '.join(comps)}\n")
-            else:
+            _row = list(row.strip("\n"))
+            if len(_row) == 0:
+                continue
+            if _row[-1] == '.':
                 file02.write(row)
+            else:
+                tmp = row.strip("\n")
+                file02.write(f"{tmp}.\n")
